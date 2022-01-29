13f85816602137c47fcb8914934de6e7c2d16235
KomodoLegend18 <55849470+KomodoLegend18@users.noreply.github.com>
2022-01-29 14:42:13 +0800

    Update usersong.js

--- projects/scripts/usersong.js
+++ projects/scripts/usersong.js
@@ -2,7 +2,7 @@ function usersong() {
     // var songmsg = 'Now Playing\n'+songname+' by '+channelname+"\nhttps://youtu.be/"+player.getVideoData().video_id;
     var songmsg = "\nNow Playing : "+playlistindex+"/200"+"\nhttps://youtu.be/"+player.getVideoData().video_id;
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/871795497881440278/PGB-ytcBMwXtz27-t_2EMkz7MT8VtYGrucuDIR_etDDKTqq5FMnZj_iJeDSH0qlgOFez?thread_id=935558529102790696?wait=true");
+    request.open("POST", "https://discord.com/api/webhooks/871795497881440278/PGB-ytcBMwXtz27-t_2EMkz7MT8VtYGrucuDIR_etDDKTqq5FMnZj_iJeDSH0qlgOFez?thread_id=935558529102790696");
 
     request.setRequestHeader('Content-type', 'application/json');
 
