b80e55e210fcaca71a54baeb5c369c75dabe3399
KomodoLegend18 <55849470+KomodoLegend18@users.noreply.github.com>
2023-10-07 09:39:24 +0800

    Revert "test"
    This reverts commit 76586a561bc2fd6e8c75ecedf760ea6c011709e3.

--- projects/Nobyar/v2/watch/param.js
+++ projects/Nobyar/v2/watch/param.js
@@ -105,8 +105,6 @@ function sync(){
             console.log("Paused",syncTime);
         }else if(respEmbed[0].fields[1].value=="Ended"){
             player.currentTime = player.duration
-            buffer_overlay.style["display"] = "grid"
-            progress_status.innerHTML = `Data Ended`
             console.log("Ended",player.duration);
         }
 
