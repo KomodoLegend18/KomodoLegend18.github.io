ce9e59f6e92b9bf659477aad00a392496a3324d7
KomodoLegend18 <andyfandy22@gmail.com>
2024-08-12 01:19:06 +0800

    random string generator

---
+++ projects/modules/random.js
@@ -0,0 +1,15 @@
+"use strict";
+
+export const randomString = (length = 6) => {
+    // Define the character set to choose from
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    // Generate a random string of the specified length
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+
+    return result;
+}
